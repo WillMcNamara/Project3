8922f914bec8e0b5fd98e4068d6c0affd6082ef3
Will M <wmcna97@gmail.com>
2019-05-30 11:27:15 -0400

    coded App.js, removed some default files, created empty pages files

--- pdba-app/src/App.js
+++ pdba-app/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Contact from "./pages/Contact";
+import ImgGallery from "./pages/ImgGallery";
+import VidGallery from "./pages/VidGallery";
+import PracticeCal from "./pages/PracticeCal";
+import BoatCal from "./pages/BoatCal";
+import Links from "./pages/Links";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/News" component={News} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/ImgGallery" component={ImgGallery} />
+          <Route exact path="/VidGallery" component={VidGallery} />
+          <Route exact path="/PracticeCal" component={PracticeCal} />
+          <Route exact path="/BoatCal" component={BoatCal} />
+          <Route exact path="/Links" component={Links} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
