e0006721cdb836e43aa88a2f141a1bc1acfd7dff
Will M <wmcna97@gmail.com>
2019-05-31 13:53:27 -0400

    styled footer, not mobile responsive

--- pdba-app/src/components/Footer/index.js
+++ pdba-app/src/components/Footer/index.js
@@ -1,26 +1,27 @@
 import React, {Component} from "react";
 import {Container, Row, Col} from "../grid";
+import "./style.css";
 
 class Footer extends Component {
     state = {}
 
     render() {
         return (
-            <footer className="font-small pt-2 bg-danger">
-                <Container>
+            <footer className="pt-2 small bg-danger">
+                <div className="container pb-0">
                     <Row>
-                        <Col size="md-4">
+                        <Col size="md-3">
                             <div className="footer-links py-2 text-center">
                                 <a className="px-2" href="#">spons1</a>
                                 <a className="px-2" href="#">spons2</a>
                                 <a className="px-2" href="#">spons3</a>
                             </div>
                         </Col>
-                        <Col size="md-4">
+                        <Col size="md-6">
                             <div className="footer-copyright text-center py-1">Philadelphia Dragon Boat Association © All Rights Reserved
                             </div>
                         </Col>
-                        <Col size="md-4">
+                        <Col size="md-3">
                             <div className="footer-links py-2 text-center">
                                 <a className="px-2" href="#">fb</a>
                                 <a className="px-2" href="#">inst</a>
@@ -28,7 +29,7 @@ class Footer extends Component {
                             </div>
                         </Col>
                     </Row>
-                </Container>
+                </div>
             </footer>
         )
     }
--- pdba-app/src/pages/Home.js
+++ pdba-app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import {Container} from "../components/grid"
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <Container>
                 Empty content
-            </div>
+            </Container>
         )
     }
 }
