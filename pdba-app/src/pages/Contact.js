2d7a51529f9e842209ff1e4c3a81f515fbec41c9
Will M <wmcna97@gmail.com>
2019-05-31 13:56:14 -0400

    added fill content to pages so they each load Nav and Footer

---
+++ pdba-app/src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import {Container} from "../components/grid"
+
+class Home extends Component {
+    render() {
+        return (
+            <Container>
+                Empty content
+            </Container>
+        )
+    }
+}
+
+export default Home;
