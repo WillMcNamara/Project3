588595bcc4341d7f11afd7bab12c192de0717751
Will M <wmcna97@gmail.com>
2019-06-04 12:38:49 -0400

    added links page

--- pdba-app/src/pages/Links.js
+++ pdba-app/src/pages/Links.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
-import {Container} from "../components/grid"
+import {Container, Row, Col} from "../components/grid"
 
 class Home extends Component {
+
+    state = {
+        links: [
+            {
+                id: 1,
+                description: "this is a link",
+                src: "www.gotothislink.com"
+            },
+            {
+                id: 1,
+                description: "this is a link",
+                src: "www.gotothislink.com"
+            },
+            {
+                id: 1,
+                description: "this is a link",
+                src: "www.gotothislink.com"
+            },
+            {
+                id: 1,
+                description: "this is a link",
+                src: "www.gotothislink.com"
+            },
+            {
+                id: 1,
+                description: "this is a link",
+                src: "www.gotothislink.com"
+            },
+        ]
+    }
+
     render() {
         return (
-            <Container>
-                Empty content
+            <Container classes="bg-white my-3 border border-dark">
+                {this.state.links.map( link => {
+                    return(
+                        <Row>
+                            <Col size="md-12">
+                                <p>{link.description}</p>
+                                <a href={link.src}>{link.src}</a>
+                            </Col>
+                        </Row>
+                    )
+                })}
             </Container>
         )
     }
