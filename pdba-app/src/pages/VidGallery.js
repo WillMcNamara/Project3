6a2ac2f34aeeac2e0b8a625a4c36b85ae2b2937d
Will M <wmcna97@gmail.com>
2019-06-04 11:15:58 -0400

    coded vidGal in form of imgGal

--- pdba-app/src/pages/VidGallery.js
+++ pdba-app/src/pages/VidGallery.js
@@ -1,13 +1,78 @@
 import React, { Component } from "react";
-import {Container} from "../components/grid"
+import {Container, Row, Col} from "../components/grid";
+import Video from "../components/Video";
 
 class Home extends Component {
-    render() {
-        return (
-            <Container>
-                Empty content
-            </Container>
-        )
+
+        state = {
+            videos: [
+                {
+                    id: 0,
+                    title: "0",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                },
+                {
+                    id: 1,
+                    title: "1",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                },
+                {
+                    id: 2,
+                    title: "2",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                },
+                {
+                    id: 3,
+                    title: "3",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                },
+                {
+                    id: 4,
+                    title: "4",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                },
+                {
+                    id: 5,
+                    title: "5",
+                    src: "https://www.youtube.com/embed/tgbNymZ7vqY"
+                }
+            ]
+        }
+
+        render() {
+            
+            const vidRows = [];
+            //format videos into rows
+            for (let i = 0; i < this.state.videos.length; i += 2) {
+                vidRows.push(
+                    <Row
+                    key={i}
+                >
+                    <Col size="md-6" classes="text-center">
+                        <Video
+                            id={this.state.videos[i].id}
+                            key={this.state.videos[i].id}
+                            title={this.state.videos[i].title}
+                            src={this.state.videos[i].src}
+                        />
+                    </Col> 
+                    
+                    <Col size="md-6" classes="text-center">
+                        <Video
+                            id={this.state.videos[i+1].id}
+                            key={this.state.videos[i+1].id}
+                            title={this.state.videos[i+1].title}
+                            src={this.state.videos[i+1].src}
+                        />
+                    </Col>
+                </Row>
+                )
+            }
+            return (
+                <Container>
+                    {vidRows}
+                </Container>
+            )
     }
 }
 
